cf612589ec2d5443b25655034fc992e1601424a1
Ramon <ramongimee@hotmail.com>
2017-12-08 12:46:45 -0200

    Criamos a Classe Page e Removemos Arquivos de Configuracao

--- Classe-Page-para-o-Template/101 - Classe Page para o Template/index.php
+++ Classe-Page-para-o-Template/101 - Classe Page para o Template/index.php
@@ -1,21 +1,19 @@
-<?php
+<?php 
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+    
+	$page = new Hcode\Page();
 
-  $page = new Page();
-
-  $page->setTpl("index");
+	$page->setTpl("index");
 
 });
 
 $app->run();
-?>
+
+ ?>
