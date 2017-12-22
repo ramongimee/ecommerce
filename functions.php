53891afc2d8a46a7cbcc075f5273e52c0959d1bf
Ramon Gimenes <ramongimee@hotmail.com>
2017-12-21 22:37:48 -0200

    Site - Produtos

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function formatPrice(float $vlprice)
+{
+
+  return number_format($vlprice,2,",",".");
+
+}
+
+
+?>
--- site.php
+++ site.php
@@ -1,12 +1,18 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Category;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
 
+  $products = Product::listall();
+
   $page = new Page();
 
-  $page->setTpl("index");
+  $page->setTpl("index",array(
+    "products"=>Product::checkList($products)
+  ));
 
 });
 
