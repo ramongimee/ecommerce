1293a6fb3921d11220e72b5079854875f04855f4
Ramon Gimenes <ramongimee@hotmail.com>
2018-01-12 20:20:42 -0200

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model{
   const SECRET = "HcodePhp7_Secret";
   const ERROR = "UserError";
   const ERROR_REGISTER = "UserErrorRegister";
+  const SUCCESS = "UserSuccess";
 
   public static function getFromSession()
   {
@@ -360,6 +361,25 @@ class User extends Model{
       return password_hash($password, PASSWORD_DEFAULT,array(
         "cost"=>12
       ));
+    }
+
+    public static function setSuccess($msg)
+    {
+      $_SESSION[User::SUCCESS] = $msg;
+    }
+
+    public static function getSuccess()
+    {
+      $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : "";
+
+      User::clearSuccess();
+
+      return $msg;
+    }
+
+    public static function clearSuccess()
+    {
+      $_SESSION[User::SUCCESS] = NULL;
     }
 }
 
