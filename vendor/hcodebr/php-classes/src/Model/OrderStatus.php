26675955737da808d61b69261b584da3e5fbbec5
Ramon Gimenes <ramongimee@hotmail.com>
2018-01-17 11:48:49 -0200

    Finalizar Pedido - Boletos

---
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer;
+use \Hcode\Model\User;
+
+class OrderStatus extends Model{
+
+  const EM_ABERTO = 1;
+  const AGUARDANDO_PAGAMENTO = 2;
+  const PAGO = 3;
+  const ENTREGUE = 4;
+
+}
+
+?>
