a7c28f2271317cbda3c0ff09e128060b14103605
Ramon Gimenes <ramongimee@hotmail.com>
2018-01-21 00:43:57 -0200

    Admin - Categorias - Busca e Paginação

--- admin-categories.php
+++ admin-categories.php
@@ -9,12 +9,37 @@ $app->get("/admin/categories", function(){
 
   User::verifyLogin();
 
-  $categories = Category::listAll();
+  $search = (isset($_GET["search"]))? $_GET["search"] : "";
+
+  $page = (isset($_GET["page"]))?(int)$_GET["page"]:1;
+
+  if($search != ""){
+
+    $pagination = Category::getPageSearch($search,$page);
+
+  }else{
+      $pagination = Category::getPage($page);
+  }
+
+
+  $pages = [];
+
+  for ($x=0; $x < $pagination["pages"]; $x++) {
+    array_push($pages,array(
+      "href"=>"/admin/users?".http_build_query(array(
+        "page"=>$x+1,
+        "search"=>$search
+      )),
+      "text"=>$x+1
+    ));
+  }
 
   $page = new PageAdmin();
 
   $page->setTpl("categories",array(
-    "categories"=>$categories
+    "categories"=>$pagination["data"],
+    "search"=>$search,
+    "pages"=>$pages
   ));
 
 });
