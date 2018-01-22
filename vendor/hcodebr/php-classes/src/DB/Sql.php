6c97b25988d1ed7f16cf7eb5097694a9d0ac3a4b
Ramon Gimenes <ramongimee@hotmail.com>
2018-01-21 23:46:35 -0200

    SQL HostGator Review

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -51,7 +51,7 @@ class Sql {
 
 	}
 
-	public function select($rawQuery, $params = array()):array
+	public function select($rawQuery, $params = array())
 	{
 
 		$stmt = $this->conn->prepare($rawQuery);
